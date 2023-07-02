3a21c2a8445abe9af772d5f4eef0fdfa6f3c6cf0
iskndrr <mohhamed.a.saeed@gmail.com>
2023-07-02 14:20:41 +0300

    login auth, validation& handling error

--- src/app/components/log-in/log-in.component.ts
+++ src/app/components/log-in/log-in.component.ts
@@ -1,19 +1,65 @@
 import { Component } from '@angular/core';
-import {FormControl, FormGroup, Validators} from '@angular/forms';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { AuthService } from 'src/app/shared/services/auth.service';
+import { MessageService } from 'primeng/api';
 
 @Component({
   selector: 'app-log-in',
   templateUrl: './log-in.component.html',
   styleUrls: ['./log-in.component.scss'],
-
+  providers: [MessageService],
 })
 export class LogInComponent {
+  isLoading: boolean = false;
+  hasError: boolean = false;
+  constructor(
+    private _authService: AuthService,
+    private messageService: MessageService,
+    private _router: Router
+  ) {}
   loginForm: FormGroup = new FormGroup({
-    email: new FormControl('',[Validators.required,Validators.email]),
-    password: new FormControl('',[Validators.required,Validators.pattern(/^[A-Z][a-z0-9]{3,10}$/)]),
+    email: new FormControl('', [Validators.required, Validators.email]),
+    password: new FormControl('', [
+      Validators.required,
+      Validators.pattern(/^[A-Z][a-z0-9]{3,10}$/),
+    ]),
   });
 
   logIn(form: FormGroup) {
     console.log(form);
+    if (this.loginForm.valid) {
+      this.isLoading = true;
+      this._authService.login(form.value).subscribe({
+        next: (res) => {
+          console.log(res);
+          this.isLoading = false;
+          this.showSuccess();
+          this._router.navigate(["/home"]);
+        },
+        error: (err) => {
+          console.log(err);
+          this.isLoading = false;
+          this.showError(err.error.message)
+        },
+        complete: () => {
+          console.log('complete');
+        },
+      });
+    }
+  }
+  showError(err: string) {
+    this.messageService.add({
+      severity: 'error',
+      summary: 'Error',
+      detail: err,
+    });
+  }
+  showSuccess() {
+    this.messageService.add({
+      severity: 'success',
+      summary: 'Success',
+      detail: 'Login Success Welcome Back',
+    });
   }
 }
--- src/app/shared/services/auth.service.ts
+++ src/app/shared/services/auth.service.ts
@@ -3,13 +3,21 @@ import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class AuthService {
+  constructor(private _httpClient: HttpClient) {}
 
-  constructor(private _httpClient:HttpClient) { }
-
-  register(data:any): Observable<any>{
-   return this._httpClient.post(`https://ecommerce.routemisr.com/api/v1/auth/signup`,data)
+  register(data: any): Observable<any> {
+    return this._httpClient.post(
+      `https://ecommerce.routemisr.com/api/v1/auth/signup`,
+      data
+    );
+  }
+  login(data: any): Observable<any> {
+    return this._httpClient.post(
+      `https://ecommerce.routemisr.com/api/v1/auth/signin`,
+      data
+    );
   }
 }
